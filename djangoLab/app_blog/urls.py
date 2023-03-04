3305052164b2a19031b55ca4552f0f9610bb5cad
YuriChmil <black4422734@gmail.com>
2023-03-04 14:43:09 +0100

    change urls

--- djangoLab/app_blog/urls.py
+++ djangoLab/app_blog/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from app_blog import views
 
+#from django.conf.urls import url Django v3
+
 urlpatterns = [
     path(r'', views.HomePageView.as_view()),
 ]
+
+
+#urlpatterns = [
+#    path(r'^$', views.home)#, name='home')
+#    path(r'^about/$', views.about, name='about'),
+#    path(r'^about/company/$', views.about_company, 
+#        name='about_company'),  
+#]
--- djangoLab/djangoLab/urls.py
+++ djangoLab/djangoLab/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('app_blog.urls')),
+# add media ????
 ]
